b78434b3218429651614d71b0d787a63b94940a4
Randy Roman <roman.randy@gmail.com>
2022-04-22 01:34:02 +0800

    Changes to be committed: New implementation using environment variables to fix failed tests using mockdata, and implemented use of data file. 	modified:   Restful Booker Test Fork.postman_collection.json 	modified:   RestfulBooker Mock Env Fork.postman_environment.json 	modified:   RestfulBooker Prod Env Fork.postman_environment.json 	modified:   newman_run.bat 	new file:   testdata.csv 	new file:   testdata.json

--- postman/newman_run.bat
+++ postman/newman_run.bat
@@ -5,4 +5,5 @@ REM Restful Booker Postman implementation using Collection variables
 REM newman run "https://www.getpostman.com/collections/8ef54e131cccd07c4dc7" -e "RestfulBooker Prod Env.postman_environment.json" -n 1 --reporters=cli,htmlextra --reporter-htmlextra-darkTheme --reporter-htmlextra-testPaging --reporter-htmlextra-title="RestfulBooker Newman API Tests" --reporter-htmlextra-logs=true --reporter-htmlextra-displayProgressBar
 
 REM Restful Booker Fork Postman implementation using Environment variables
-newman run "https://www.getpostman.com/collections/4a69baba9fdcc06f68fc" -e "RestfulBooker Prod Env Fork.postman_environment.json" -n 1 --reporters=cli,htmlextra --reporter-htmlextra-darkTheme --reporter-htmlextra-testPaging --reporter-htmlextra-title="RestfulBooker Newman API Tests" --reporter-htmlextra-logs=true --reporter-htmlextra-displayProgressBar
+newman run "https://www.getpostman.com/collections/4a69baba9fdcc06f68fc" -e "RestfulBooker Prod Env Fork.postman_environment.json" -d "testdata.json" --reporters=cli,htmlextra --reporter-htmlextra-darkTheme --reporter-htmlextra-testPaging --reporter-htmlextra-title="RestfulBooker Newman API Tests" --reporter-htmlextra-logs=true --reporter-htmlextra-displayProgressBar
+
